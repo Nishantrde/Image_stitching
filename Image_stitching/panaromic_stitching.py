6470425e7e648f3f034ccc64dc553f61017eb913
Nishant <nishant2024rde@gmail.com>
2026-01-27 00:55:37 -0800

    stitching images

--- Image_stitching/panaromic_stitching.py
+++ Image_stitching/panaromic_stitching.py
@@ -7,4 +7,24 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 image_path = glob.glob(os.path.join(script_dir, "imgs/*.jpeg"))
 print(image_path)
+images = []
+
+for image in  image_path:
+    img =  cv2.imread(image)
+    images.append(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+
+imageSticher = cv2.Stitcher_create()
+
+error, stitched_img = imageSticher.stitch(images)
+
+if not error:
+        cv2.imwrite("stitched_images.png", stitched_img)
+        cv2.imshow("stitched images", stitched_img)
+        cv2.waitKey(0)
+
+
+
+
 
