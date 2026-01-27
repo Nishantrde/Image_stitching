bfb8ca1f904317dad66dcd67b604b0034e586629
Nishant <nishant2024rde@gmail.com>
2026-01-27 00:41:10 -0800

    extract images from folder

--- Image_stitching/panaromic_stitching.py
+++ Image_stitching/panaromic_stitching.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 import glob
 import imutils
+import os
 
-image_path = glob.glob()
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = glob.glob(os.path.join(script_dir, "imgs/*.jpeg"))
+print(image_path)
 
